762ca932dde85e9295c9744e8e9cef4aed396684
Ilya S. Zobenko <ilya@zobenko.ru>
2020-01-13 22:56:46 +0300

    Update project

--- assets/js/dredd.js
+++ assets/js/dredd.js
@@ -2,5 +2,5 @@ const hooks = require('hooks');
 const getenv = require('getenv');
 
 hooks.beforeEach((transaction) => {
-  transaction.request.headers["X-AUTH-TOKEN"] = getenv("DREDD_AUTH_TOKEN")
+  transaction.request.headers["X-AUTH-TOKEN"] = "yV_Y_mSKBkbCbdsw7S64oVnm5tGRQoQRuQNPoHPCU40"
 });
